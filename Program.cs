153f0fe186c77e18049797c9df8f309d46b9487c
Chris <chrispackett1998@gmail.com>
2018-09-17 19:08:35 -0400

    added new SQL queries with Entity Framework

--- Program.cs
+++ Program.cs
@@ -11,20 +11,45 @@ namespace orm_safari
 
             using (var db = new SafariVacationContext())
             {
-                //Prompt and get seen animal from user.
-                Console.WriteLine("What is one animal that you have seen today?");
-                var species = Console.ReadLine();
+                // //Prompt and get seen animal from user.
+                // Console.WriteLine("What is one animal that you have seen today?");
+                // var species = Console.ReadLine();
 
-                //Create a new SeenAnimals instance called newSpecies and assign it a species.
-                var newSpecies = new SeenAnimals { Species = species };
+                // //Create a new SeenAnimals instance called newSpecies and assign it a species.
+                // var newSpecies = new SeenAnimals { Species = species };
 
-                //Add the new SeenAnimals instance to the SeenAnimalsTable in the SafariVacation database.
-                //Technically, we are just adding the newSpecies to a DbSet<SeenAnimals> in C#, which
-                //connects to the SQL server and adds a new row into the SeenAnimalsTable.
-                db.SeenAnimalsTable.Add(newSpecies);
+                // if (species == "")
+                // {
+                //     //If no user input, display message below.
+                //     Console.WriteLine("Nothing to add");
+                // }
+                // else
+                // {
+                //     //Add the new SeenAnimals instance to the SeenAnimalsTable in the SafariVacation database.
+                //     //Technically, we are just adding the newSpecies to a DbSet<SeenAnimals> in C#, which
+                //     //connects to the SQL server and adds a new row into the SeenAnimalsTable.
+                //     db.SeenAnimalsTable.Add(newSpecies);
 
-                //commits changes from C# to the database.
-                db.SaveChanges();
+                //     //commits changes from C# to the database.
+                //     db.SaveChanges();
+                // }
+
+                // //Display all of the animals that the user has seen
+                // var allSeenAnimals = db.SeenAnimalsTable;
+
+                // foreach (var animal in allSeenAnimals)
+                // {
+                //     Console.WriteLine($"Animal seen: {animal.Species}");
+                // }
+
+                // //Update the CountOfTimesSeen and LocationOfLastSeen for an animal
+                // var animalToUpdate = db.SeenAnimalsTable.FirstOrDefault(animal => animal.Id == 4);
+                // animalToUpdate.CountOfTimesSeen = 1;
+                // animalToUpdate.LocationOfLastSeen = "Grasslands";
+
+                // db.SaveChanges();
+
+                
             }
         }
     }
