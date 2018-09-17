5a24fbc8f58162c4fe7e8676e7fe01b70ab6f614
Chris <chrispackett1998@gmail.com>
2018-09-17 13:39:11 -0400

    first commit, added dbcontext

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace orm_safari
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ SafariVacationContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace orm_safari
+{
+    public partial class SafariVacationContext : DbContext
+    {
+        public SafariVacationContext()
+        {
+        }
+
+        public SafariVacationContext(DbContextOptions<SafariVacationContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseNpgsql("server=localhost;database=safari_vacation");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {}
+    }
+}
