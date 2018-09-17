be677672337b89008542e38749f6042909734370
Chris <chrispackett1998@gmail.com>
2018-09-17 15:43:05 -0400

    added a way for users to add animals to the SeenAnimalsTable

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using orm_safari.models;
 
 namespace orm_safari
 {
@@ -6,7 +8,24 @@ namespace orm_safari
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            using (var db = new SafariVacationContext())
+            {
+                //Prompt and get seen animal from user.
+                Console.WriteLine("What is one animal that you have seen today?");
+                var species = Console.ReadLine();
+
+                //Create a new SeenAnimals instance called newSpecies and assign it a species.
+                var newSpecies = new SeenAnimals { Species = species };
+
+                //Add the new SeenAnimals instance to the SeenAnimalsTable in the SafariVacation database.
+                //Technically, we are just adding the newSpecies to a DbSet<SeenAnimals> in C#, which
+                //connects to the SQL server and adds a new row into the SeenAnimalsTable.
+                db.SeenAnimalsTable.Add(newSpecies);
+
+                //commits changes from C# to the database.
+                db.SaveChanges();
+            }
         }
     }
 }
--- SafariVacationContext.cs
+++ SafariVacationContext.cs
@@ -20,14 +20,13 @@ namespace orm_safari
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
                 optionsBuilder.UseNpgsql("server=localhost;database=safari_vacation");
             }
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-        }
+        {}
         
         public DbSet<SeenAnimals> SeenAnimalsTable { get; set; }
     }
