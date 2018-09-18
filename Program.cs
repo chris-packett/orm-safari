1d5c3059a3aad1711a0fd6bfa3681c4d92a7d306
Chris <chrispackett1998@gmail.com>
2018-09-17 20:12:18 -0400

    added special commands!

--- Program.cs
+++ Program.cs
@@ -15,6 +15,7 @@ namespace orm_safari
                 Console.WriteLine("(A)dd an animal.");
                 Console.WriteLine("(D)isplay all animals from the Safari thus far.");
                 Console.WriteLine("(U)pdate an animal.");
+                Console.WriteLine("(S)pecial commands.");
 
                 var userAction = Console.ReadLine();
                 userAction = userAction.ToUpper();
@@ -30,6 +31,9 @@ namespace orm_safari
                     case "U":
                         updateAnimal();
                         break;
+                    case "S":
+                        specialPrompts();
+                        break;
                     default:
                         Console.WriteLine("Please choose a valid option!");
                         break;
@@ -38,12 +42,12 @@ namespace orm_safari
                 void addAnimal() 
                 {
                     Console.WriteLine("Please tell me what animal you want to add!");
-                    var _species = Console.ReadLine();
+                    var _animalSpecies = Console.ReadLine();
 
                     //Create a new SeenAnimals instance called newSpecies and assign it a species.
-                    var _newSpecies = new SeenAnimals { Species = _species };
+                    var _newSpecies = new SeenAnimals { Species = _animalSpecies };
 
-                    if (_species != "")
+                    if (_animalSpecies != "")
                     {
                         //Add the new SeenAnimals instance to the SeenAnimalsTable in the SafariVacation database.
                         //Technically, we are just adding the newSpecies to a DbSet<SeenAnimals> in C#, which
@@ -89,6 +93,91 @@ namespace orm_safari
 
                     //commits changes from C# to the database.
                     db.SaveChanges();
+                }
+
+                void displayAnimalsFromLocation()
+                {
+                    Console.WriteLine("From what location would you like to display all of the animals?");
+                    var _location = Console.ReadLine();
+
+                    var animalsToDisplay = db.SeenAnimalsTable.Where(animal => animal.LocationOfLastSeen == _location);
+
+                    Console.WriteLine($"Animals from {_location}");
+                    foreach (var _animalSpecies in animalsToDisplay)
+                    {
+                        Console.WriteLine($"Animal seen: {_animalSpecies.Species}");
+                    }
+                }
+
+                void removeAnimalsFromLocation()
+                {
+                    Console.WriteLine("From what location would you like to remove all of the animals?");
+                    var _location = Console.ReadLine();
+
+                    var animalsToRemove = db.SeenAnimalsTable.Where(animal => animal.LocationOfLastSeen == _location);
+
+                    foreach (var _animalSpecies in animalsToRemove)
+                    {
+                        db.SeenAnimalsTable.Remove(_animalSpecies);
+                    }
+
+                    db.SaveChanges();
+                }
+
+                void getTotalCountSeen()
+                {
+                    var _totalCount = 0;
+                    var _allSeenAnimals = db.SeenAnimalsTable;
+
+                    foreach (var _animal in _allSeenAnimals)
+                    {
+                        _totalCount += _animal.CountOfTimesSeen;
+                    }
+                    
+                    Console.WriteLine($"You have seen a total of {_totalCount} animals!");
+                }
+
+                void getTotalCountSeenWizardsOfOzEdition()
+                {
+                    var _totalCount = 0;
+                    var _allWizardOfOzAnimals = db.SeenAnimalsTable.Where(animal => animal.Species == "Lion" || animal.Species == "Tiger" || animal.Species == "Bear");
+
+                    foreach (var _animal in _allWizardOfOzAnimals)
+                    {
+                        _totalCount += _animal.CountOfTimesSeen;
+                    }
+
+                    Console.WriteLine($"You have seen a total of {_totalCount} animals of Wizard type!");
+                }
+
+                void specialPrompts()
+                {
+                    Console.WriteLine("(D)isplay all animals from a specific location.");
+                    Console.WriteLine("(R)emove all animals from a specific location.");
+                    Console.WriteLine("(G)et total count of all animals you have seen.");
+                    Console.WriteLine("(W)izards of Oz edition: total count of lions, tigers, and bears seen.");
+
+                    var specialUserAction = Console.ReadLine();
+                    specialUserAction = specialUserAction.ToUpper();
+                    
+                    switch (specialUserAction) 
+                    {
+                        case "D":
+                            displayAnimalsFromLocation();
+                            break;
+                        case "R":
+                            removeAnimalsFromLocation();
+                            break;
+                        case "G":
+                            getTotalCountSeen();
+                            break;
+                        case "W":
+                            getTotalCountSeenWizardsOfOzEdition();
+                            break;
+                        default:
+                            Console.WriteLine("Please choose a valid option!");
+                            break;
+                    }
                 }
             }
         }
